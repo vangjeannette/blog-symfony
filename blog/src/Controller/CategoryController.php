2e2582fc346b9f61892bf3355bbfc86aa95f335b
Vang Jeannette <vangjeannette@gmail.com>
2019-05-19 17:37:26 +0200

    création du form version noob

---
+++ blog/src/Controller/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category"
+     * @param Request $request
+     * @return Response A response
+     */
+    public function addNewCategory(Request $request): Response
+    {
+        $category = new Category();
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data= $form->getData();
+            $categoryManager = $this->getDoctrine()->getManager();
+            $categoryManager->persist($data);
+            $categoryManager->flush();
+            return $this->redirectToRoute('category');
+
+        }
+
+        $this->render('category/index.html.twig', [
+            'form' => $form->createView(),
+            'categories' => $categories,
+    ]);
+
+    }
+}
