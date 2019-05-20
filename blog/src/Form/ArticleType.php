5321c16dd0fb78c1d06044ed8b74c63aae5fc777
Vang Jeannette <vangjeannette@gmail.com>
2019-05-20 18:34:17 +0200

    modification de la fonction buildForm pour la quête 13, afin de permettre le multiple select, même s'il était déjà possible de le faire car la relation tag et article avait été faite avant le crud

--- blog/src/Form/ArticleType.php
+++ blog/src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,8 +17,15 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
-            ->add('tags', null, ['choice_label' =>'name'])
-        ;
+        //    ->add('tags', null, ['choice_label' =>'name']) la relation entre tag et article avait été faite avant le crud
+        //      je mets donc la fonction de base qui fonctionnait avec le select multiple en commentaire
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
